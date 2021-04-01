05ba19b3ad90c47692d83a71765ad2c325015a76
agumy <nov13sh@gmail.com>
2021-04-01 23:29:16 +0900

    add

--- 5/easy-server.js
+++ 5/easy-server.js
@@ -24,12 +24,3 @@ const server = http
     }
   })
   .listen(3000);
-
-http
-  .request("http://localhost:3000/api/todos", (res) => {
-    let responseData = "";
-    console.log("statusCode", res.statusCode);
-    res.on("data", (chunk) => (responseData += chunk));
-    res.on("end", () => console.log("responseData", JSON.parse(responseData)));
-  })
-  .end();
